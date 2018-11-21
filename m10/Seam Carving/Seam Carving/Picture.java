edc56c80752577fac7b21ea8e29d3e5e195eabfc
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-11-21 21:45:16 +0530

    "ads2_m10_a1_1: 9/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ m10/Seam Carving/Seam Carving/Picture.java
@@ -0,0 +1,440 @@
+import java.awt.Color;
+import java.awt.FileDialog;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+
+/**
+ * {Class of picture}.
+ */
+public final class Picture implements ActionListener {
+    /**
+     * {the rasterized image}.
+     */
+    private BufferedImage image;
+    /**
+     * {on-screen view}.
+     */
+    private JFrame frame;
+    /**
+     * {name of file}.
+     */
+    private String filename;
+    /**
+     * {location of origin}.
+     */
+    private boolean isOriginUpperLeft = true;
+    /**
+     * {width and height}.
+     */
+    private final int width, height;
+
+    /**
+     * Creates a {@code width}-by-{@code height} picture.
+     * with {@code width} columns and {@code height} rows,
+     * where each pixel is black.
+     * @param width1 the width of the picture
+     * @param height1 the height of the picture
+     */
+    Picture(final int width1, final int height1) {
+        this.width  = width1;
+        this.height = height1;
+        image = new BufferedImage(
+            width1, height1, BufferedImage.TYPE_INT_RGB);
+    }
+
+    /**
+     * Creates a new picture that is a deep copy of the argument picture.
+     *
+     * @param  picture the picture to copy
+     */
+    Picture(final Picture picture) {
+        width  = picture.width();
+        height = picture.height();
+        image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        filename = picture.filename;
+        isOriginUpperLeft = picture.isOriginUpperLeft;
+        for (int col = 0; col < width(); col++) {
+            for (int row = 0; row < height(); row++) {
+                image.setRGB(col, row, picture.image.getRGB(col, row));
+            }
+        }
+    }
+
+    /**
+     * Creates a picture by reading an image from a file or URL.
+     * @param  filename1 the name of the file (.png, .gif, or .jpg) or URL.
+     */
+    Picture(final String filename1) {
+        this.filename = filename1;
+        try {
+            File file = new File(filename1);
+            if (file.isFile()) {
+                image = ImageIO.read(file);
+            } else {
+                URL url = getClass().getResource(filename1);
+                if (url == null) {
+                    url = new URL(filename1);
+                }
+                image = ImageIO.read(url);
+            }
+
+            if (image == null) {
+                throw new IllegalArgumentException(
+                    "could not read image file: " + filename1);
+            }
+            width  = image.getWidth(null);
+            height = image.getHeight(null);
+        } catch (IOException ioe) {
+            throw new IllegalArgumentException(
+                "could not open image file: " + filename1, ioe);
+        }
+    }
+
+    /**
+     * Creates a picture by reading the image from a PNG, GIF, or JPEG file.
+     *
+     * @param file the file
+     */
+    Picture(final File file) {
+        try {
+            image = ImageIO.read(file);
+        } catch (IOException ioe) {
+            throw new IllegalArgumentException(
+                "could not open file: " + file, ioe);
+        }
+        if (image == null) {
+            throw new IllegalArgumentException(
+                "could not read file: " + file);
+        }
+        width  = image.getWidth(null);
+        height = image.getHeight(null);
+        filename = file.getName();
+    }
+
+    /**
+     * Returns a {@link JLabel} containing this picture,
+     * for embedding in a {@link JPanel},
+     * {@link JFrame} or other GUI widget.
+     * @return the {@code JLabel}
+     */
+    public JLabel getJLabel() {
+        if (image == null) {
+            return null;
+        }
+        ImageIcon icon = new ImageIcon(image);
+        return new JLabel(icon);
+    }
+
+    /**
+      * Sets the origin to be the upper left pixel. This is the default.
+      */
+    public void setOriginUpperLeft() {
+        isOriginUpperLeft = true;
+    }
+
+    /**
+      * Sets the origin to be the lower left pixel.
+      */
+    public void setOriginLowerLeft() {
+        isOriginUpperLeft = false;
+    }
+
+    /**
+      * Displays the picture in a window on the screen.
+      */
+    public void show() {
+        if (frame == null) {
+            frame = new JFrame();
+            JMenuBar menuBar = new JMenuBar();
+            JMenu menu = new JMenu("File");
+            menuBar.add(menu);
+            JMenuItem menuItem1 = new JMenuItem(" Save...   ");
+            menuItem1.addActionListener(this);
+            menuItem1.setAccelerator(KeyStroke.getKeyStroke(
+                                         KeyEvent.VK_S,
+                        Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()));
+            menu.add(menuItem1);
+            frame.setJMenuBar(menuBar);
+
+            frame.setContentPane(getJLabel());
+            // f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            if (filename == null) {
+                frame.setTitle(width + "-by-" + height);
+            } else {
+                frame.setTitle(filename);
+            }
+            frame.setResizable(false);
+            frame.pack();
+            frame.setVisible(true);
+        }
+        // draw
+        frame.repaint();
+    }
+
+    /**
+      * Returns the height of the picture.
+      *
+      * @return the height of the picture (in pixels)
+      */
+    public int height() {
+        return height;
+    }
+
+    /**
+      * Returns the width of the picture.
+      *
+      * @return the width of the picture (in pixels)
+      */
+    public int width() {
+        return width;
+    }
+
+    /**
+     * { function_description }.
+     *
+     * @param      row   The row
+     */
+    private void validateRowIndex(final int row) {
+        if (row < 0 || row >= height()) {
+            throw new IllegalArgumentException(
+                "row index must be between 0 and "
+                +  (height() - 1) + ": " + row);
+        }
+    }
+
+    /**
+     * { function_description }.
+     *
+     * @param      col   The col
+     */
+    private void validateColumnIndex(final int col) {
+        if (col < 0 || col >= width()) {
+            throw new IllegalArgumentException(
+                "column index must be between 0 and "
+                + (width() - 1) + ": " + col);
+        }
+    }
+
+    /**
+      * Returns the color of pixel ({@code col},
+      * {@code row}) as a {@link java.awt.Color}.
+      * @param col the column index
+      * @param row the row index
+      * @return the color of pixel ({@code col},
+      * {@code row})
+      */
+    public Color get(final int col, final int row) {
+        validateColumnIndex(col);
+        validateRowIndex(row);
+        int rgb = getRGB(col, row);
+        return new Color(rgb);
+    }
+
+    /**
+      * Returns the color of pixel ({@code col}, {@code row}) as an {@code int}.
+      * Using this method can be more efficient than
+      * {@link #get(int, int)} because it does not
+      * create a {@code Color} object.
+      * @param col the column index
+      * @param row the row index
+      * @return the integer representation of the color of pixel
+      * ({@code col}, {@code row})
+      */
+    public int getRGB(final int col, final int row) {
+        validateColumnIndex(col);
+        validateRowIndex(row);
+        if (isOriginUpperLeft) {
+            return image.getRGB(col, row);
+        } else {
+            return image.getRGB(col, height - row - 1);
+        }
+    }
+
+    /**
+      * Sets the color of pixel ({@code col}.
+      * {@code row}) to given color.
+      * @param col the column index
+      * @param row the row index
+      * @param color the color
+      */
+    public void set(final int col, final int row, final Color color) {
+        validateColumnIndex(col);
+        validateRowIndex(row);
+        if (color == null) {
+            throw new IllegalArgumentException(
+                "color argument is null");
+        }
+        int rgb = color.getRGB();
+        setRGB(col, row, rgb);
+    }
+
+    /**
+      * Sets the color of pixel ({@code col}, {@code row}) to given color.
+      * @param col the column index
+      * @param row the row index
+      * @param rgb the integer representation of the color
+      */
+    public void setRGB(final int col, final int row, final int rgb) {
+        validateColumnIndex(col);
+        validateRowIndex(row);
+        if (isOriginUpperLeft) {
+            image.setRGB(col, row, rgb);
+        } else {
+            image.setRGB(col, height - row - 1, rgb);
+        }
+    }
+
+    /**
+      * Returns true if this picture is equal to the argument picture.
+      *
+      * @param other the other picture
+      * @return {@code true} if this picture
+      * is the same dimension as {@code other}
+      * and if all pixels have the same color;
+      * {@code false} otherwise
+      */
+    public boolean equals(final Object other) {
+        if (other == this) {
+            return true;
+        }
+        if (other == null) {
+            return false;
+        }
+        if (other.getClass() != this.getClass()) {
+            return false;
+        }
+        Picture that = (Picture) other;
+        if (this.width()  != that.width()) {
+            return false;
+        }
+        if (this.height() != that.height()) {
+            return false;
+        }
+        for (int col = 0; col < width(); col++) {
+            for (int row = 0; row < height(); row++) {
+                if (this.getRGB(col, row) != that.getRGB(col, row)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+      * Returns a string representation of this picture.
+      * The result is a width-by-height matrix of pixels,
+      * where the color of a pixel is represented using
+      * 6 hex digits to encode
+      * the red, green, and blue components.
+      * @return a string representation of this picture
+      */
+    public String toString() {
+        final int x = 0xFFFFFF;
+        StringBuilder sb = new StringBuilder();
+        sb.append(
+            width + "-by-" + height + " picture (RGB values given in hex)\n");
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                int rgb = 0;
+                if (isOriginUpperLeft) {
+                    rgb = image.getRGB(col, row);
+                } else {
+                    rgb = image.getRGB(col, height - row - 1);
+                }
+                sb.append(String.format("#%06X ", rgb & x));
+            }
+            sb.append("\n");
+        }
+        return sb.toString().trim();
+    }
+
+    /**
+     * This operation is not supported because pictures are mutable.
+     *
+     * @return does not return a value
+     * @throws UnsupportedOperationException if called
+     */
+    public int hashCode() {
+        throw new UnsupportedOperationException(
+            "hashCode() is not supported because pictures are mutable");
+    }
+
+    /**
+      * Saves the picture to a file in either PNG or JPEG format.
+      * The filetype extension must be either .png or .jpg.
+      *
+      * @param name the name of the file
+      * @throws IllegalArgumentException if {@code name} is {@code null}
+      */
+    public void save(final String name) {
+        if (name == null) {
+            throw new IllegalArgumentException(
+                "argument to save() is null");
+        }
+        save(new File(name));
+        filename = name;
+    }
+
+    /**
+      * Saves the picture to a file in a PNG or JPEG image format.
+      *
+      * @param  file the file
+      * @throws IllegalArgumentException if {@code file} is {@code null}
+      */
+    public void save(final File file) {
+        if (file == null) {
+            throw new IllegalArgumentException(
+                "argument to save() is null");
+        }
+        filename = file.getName();
+        if (frame != null) {
+            frame.setTitle(filename);
+        }
+        String suffix = filename.substring(filename.lastIndexOf('.') + 1);
+        if ("jpg".equalsIgnoreCase(suffix)
+                || "png".equalsIgnoreCase(suffix)) {
+            try {
+                ImageIO.write(image, suffix, file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Error: filename must end in .jpg or .png");
+        }
+    }
+
+    /**
+      * Opens a save dialog box when the user selects "Save As" from the menu.
+      */
+    @Override
+    public void actionPerformed(final ActionEvent e) {
+        FileDialog chooser = new FileDialog(frame,
+                                            "Use a .png or .jpg extension",
+                                            FileDialog.SAVE);
+        chooser.setVisible(true);
+        if (chooser.getFile() != null) {
+            save(chooser.getDirectory() + File.separator + chooser.getFile());
+        }
+    }
+}
+
+
+
+
+
+
---
+++ m10/Seam Carving/Seam Carving/SeamCarver.java
@@ -0,0 +1,295 @@
+import java.awt.Color;
+/**
+ * Class for seam carver.
+ */
+public class SeamCarver {
+    /**
+     * {Border Energy Value}.
+     */
+    private static final double BORDER_VALUE = 1000;
+    /**
+     * {Picture Object}.
+     */
+    private Picture pic;
+
+    /**
+     * Constructs the object.
+     *
+     * @param      picture  The picture
+     */
+    SeamCarver(final Picture picture) {
+        if (picture == null) {
+            throw new java.lang.IllegalArgumentException("picture is null");
+        }
+        this.pic = picture;
+    }
+
+
+    /**
+     * {current picture}.
+     *
+     * @return     {Picture}
+     */
+    public Picture picture() {
+        return this.pic;
+    }
+
+    /**
+     * {width of current picture}.
+     *
+     * @return     {Integer}
+     */
+    public int width() {
+        return this.pic.width();
+    }
+
+    /**
+     * {height of current picture}.
+     *
+     * @return     {Integer}
+     */
+    public int height() {
+        return this.pic.height();
+    }
+
+
+    /**
+     * {energy of pixel at column x and row y}.
+     *
+     * @param      x     {Column}
+     * @param      y     {Row}
+     *
+     * @return     {Double}
+     */
+    public  double energy(final int x, final int y) {
+        int w = width() - 1, h = height() - 1;
+        if (x < 0 || x > w || y < 0 || y > h) {
+            throw new java.lang.IllegalArgumentException(
+                "IllegalArgumentException");
+        }
+        if (x == 0 || x == w ||  y == 0 || y == h) {
+            return BORDER_VALUE;
+        }
+        return internalEnergy(x, y);
+    }
+
+
+    /**
+     * {energy of pixel at column x and row y not on border}.
+     *
+     * @param      x     {Column}
+     * @param      y     {Row}
+     *
+     * @return     {Double}
+     */
+    private double internalEnergy(final int x, final int y) {
+        Color left = this.pic.get(x - 1, y);
+        Color right = this.pic.get(x + 1, y);
+        Color up = this.pic.get(x, y - 1);
+        Color down = this.pic.get(x, y + 1);
+        return Math.sqrt(gradient(left, right) + gradient(up, down));
+    }
+
+    /**
+     * {Method to find the gradient}.
+     *
+     * @param      one   One
+     * @param      two   Two
+     *
+     * @return     {Double}
+     */
+    private double gradient(final Color one, final Color two) {
+        double red = one.getRed() - two.getRed();
+        double green = one.getGreen() - two.getGreen();
+        double blue = one.getBlue() - two.getBlue();
+        return red * red + green * green + blue * blue;
+    }
+
+    /**
+     * Gets the energy matrix.
+     *
+     * @return     The energy matrix.
+     */
+    private double[][] getEnergyMatrix() {
+        double[][] energies = new double[height()][width()];
+        for (int i = 0; i < height(); i++) {
+            for (int j = 0; j < width(); j++) {
+                energies[i][j] = energy(j, i);
+            }
+        }
+        return energies;
+    }
+
+
+    /**
+     * {pass through an array and mark the.
+     * shorthest distance from top to entry}
+     *
+     * @param      array  The array
+     */
+    private void topologicalSort(final double[][] array) {
+        int h = array.length;
+        int w = array[0].length;
+        for (int row = 1; row < h; row++) {
+            for (int col = 0; col < w; col++) {
+                double temp = array[row - 1][col];
+                double min = 0;
+                if (col == 0) {
+                    min = temp;
+                } else {
+                    min = Math.min(temp, array[row - 1][col - 1]);
+                }
+
+                if (col != (w - 1)) {
+                    min = Math.min(min, array[row - 1][col + 1]);
+                } else {
+                    min = min;
+                }
+                array[row][col] += min;
+            }
+        }
+
+    }
+    /**
+     * {Method to transpose the grid}.
+     *
+     * @param      array  The array
+     *
+     * @return     {2-D Double Array}
+     */
+    private double[][] transposeGrid(final double[][] array) {
+        int h = array.length;
+        int w = array[0].length;
+        double[][] tempArray = new double[w][h];
+        for (int row = 0; row < h; row++) {
+            for (int col = 0; col < w; col++) {
+                tempArray[col][row] = array[row][col];
+            }
+        }
+        return tempArray;
+    }
+
+    /**
+     * {Method to find the minimum vertical path}.
+     *
+     * @param      array  The array
+     *
+     * @return     {1-D Integer array}
+     */
+    private int[] minVerticalPath(final double[][] array) {
+        int h = array.length, w = array[0].length;
+        int[] path = new int[h];
+
+        topologicalSort(array);
+
+        path[h - 1] = 0;
+        for (int i = 0; i < w; i++) {
+            if (array[h - 1][i] < array[h - 1][path[h - 1]]) {
+                path[h - 1] = i;
+            }
+        }
+        for (int row = h - 2; row >= 0; row--) {
+            int col = path[row + 1];
+            path[row] = col;
+            if (col > 0 && array[row][col - 1] < array[row][path[row]]) {
+                path[row] = col - 1;
+            }
+            if (col < (w - 2) && array[row][col + 1] < array[row][path[row]]) {
+                path[row] = col + 1;
+            }
+        }
+        return path;
+    }
+
+    /**
+     * {sequence of indices for horizontal seam}.
+     *
+     * @return     {1-D Integer array}
+     */
+    public int[] findHorizontalSeam() {
+        double[][] transposeEnergies = transposeGrid(getEnergyMatrix());
+        return minVerticalPath(transposeEnergies);
+    }
+
+    /**
+     * {sequence of indices for vertical seam}.
+     *
+     * @return     {1-D Integer array}
+     */
+    public int[] findVerticalSeam() {
+        double[][] normalEnergies = getEnergyMatrix();
+        return minVerticalPath(normalEnergies);
+    }
+
+    /**
+     * Removes a horizontal seam.
+     *
+     * @param      seam  The seam
+     */
+    public void removeHorizontalSeam(final int[] seam) {
+        if (height() <= 1 || !isValid(seam, width(), height() - 1)) {
+            throw new java.lang.IllegalArgumentException(
+                "IllegalArgumentException");
+        }
+        Picture picture1 = new Picture(width(), height() - 1);
+        for (int w = 0; w < width(); w++) {
+            for (int h = 0; h < seam[w]; h++) {
+                picture1.set(w, h, this.pic.get(w, h));
+            }
+
+            for (int h = seam[w] + 1; h < height(); h++) {
+                picture1.set(w, h - 1, this.pic.get(w, h));
+            }
+        }
+        this.pic = picture1;
+    }
+
+
+    /**
+     * Removes a vertical seam.
+     *
+     * @param      seam  The seam
+     */
+    public void removeVerticalSeam(final int[] seam) {
+        if (width() <= 1 || !isValid(seam, height(), width())) {
+            throw new java.lang.IllegalArgumentException(
+                "IllegalArgumentException");
+        }
+        Picture picture2 = new Picture(width() - 1, height());
+        for (int h = 0; h < height(); h++) {
+            for (int w = 0; w < seam[h]; w++) {
+                picture2.set(w, h, this.pic.get(w, h));
+            }
+            for (int w = seam[h] + 1; w < width(); w++) {
+                picture2.set(w - 1, h, this.pic.get(w, h));
+            }
+        }
+        this.pic = picture2;
+    }
+
+    /**
+     * Determines if valid.
+     *
+     * @param      a      {Integer array}
+     * @param      len    The length
+     * @param      range  The range
+     *
+     * @return     True if valid, False otherwise.
+     */
+    private boolean isValid(final int[] a,
+                            final int len, final int range) {
+        if (a == null) {
+            return false;
+        }
+        if (a.length != len || a[0] < 0 || a[0] > range) {
+            return false;
+        }
+        for (int i = 1; i < len; i++) {
+            if (a[i] < Math.max(0, a[i - 1] - 1)
+                    || a[i] > Math.min(range, a[i - 1] + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
