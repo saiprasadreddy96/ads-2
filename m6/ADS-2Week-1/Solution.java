dd0223caaa980e8933355ce940617b90b916f331
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-12-20 11:50:52 +0530

    "ads2_m6_a1_1: 4/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m6/ADS-2Week-1/Solution.java
+++ m6/ADS-2Week-1/Solution.java
@@ -1,80 +1,234 @@
-
-import java.util.Scanner;
-
+import java.util.Arrays;
+import java.util.Hashtable;
+/**
+ * Class for page rank.
+ */
+class PageRank {
+    /**
+     * { Digraph object }.
+     */
+    private Digraph pggraph;
+    /**
+     * { Arrays to store previous values }.
+     */
+    private double[] prval;
+    /**
+     * { Arrays to store current values }.
+     */
+    private double[] nowval;
+    /**
+     * Constructs the object.
+     *
+     * @param      graph  The graph
+     */
+    PageRank(final Digraph graph) {
+        this.pggraph = graph;
+        prval = new double[pggraph.V()];
+        for (int y = 0; y < prval.length; y++) {
+            prval[y] = (1.0 / (pggraph.V()));
+        }
+        for (int z = 0; z < pggraph.V(); z++) {
+            if (pggraph.outdegree(z) == 0) {
+                for (int b = 0; b < pggraph.V(); b++) {
+                    if (b != z) {
+                        pggraph.addEdge(z, b);
+                    }
+                }
+            }
+        }
+        nowval = new double[pggraph.V()];
+        updatingprvals();
+    }
+    /**
+     * { updatingprvals }.
+     */
+    void updatingprvals() {
+        final int thou = 1001;
+        for (int i = 1; i < thou; i++) {
+            // System.out.println("iteration number - " + i);
+            for (int j = 0; j < pggraph.V(); j++) {
+                update(j);
+            }
+            prval = Arrays.copyOf(nowval, nowval.length);
+            // if(Arrays.equals(prval, nowval)) {
+            //      break;
+            // }
+        }
+    }
+    /**
+     * Gets the pr.
+     *
+     * @param      v     { parameter_description }
+     *
+     * @return     The pr.
+     */
+    double getPR(final int v) {
+        return nowval[v];
+    }
+    /**
+     * { update function }.
+     *
+     * @param      v     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    double update(final int v) {
+        double testprval = 0.0;
+        if (pggraph.indegree(v) == 0) {
+            nowval[v] = 0.0;
+            return nowval[v];
+        }
+        for (Integer eachadj : pggraph.reverse().adj(v)) {
+            testprval = testprval + (
+                            prval[eachadj] / pggraph.outdegree(
+                                eachadj));
+        }
+        // System.out.println(testprval);
+        nowval[v] = testprval;
+        return nowval[v];
+    }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        String str = "";
+        for (int l = 0; l < nowval.length; l++) {
+            str = str + l + " - " + nowval[l] + "\n";
+        }
+        return str;
+    }
+}
+/**
+ * Class for web search.
+ */
 class WebSearch {
+    /**
+     * { pgrankobject }.
+     */
+    private PageRank pgrankobjinclass;
+    /**
+     * { Hashtable }.
+     */
+    private Hashtable<String, Bag<Integer>> hashtableobj;
+    /**
+     * Constructs the object.
+     *
+     * @param      rankobj   The rankobj
+     * @param      filename  The filename
+     */
+    WebSearch(final PageRank rankobj, final String filename) {
+        pgrankobjinclass = rankobj;
+        In newfile = new In(filename);
+        hashtableobj = new Hashtable<>();
+        while (newfile.hasNextLine()) {
+            String eachline = newfile.readLine();
+            String[] tokens = eachline.split(":");
+            for (String word : tokens[1].split(" ")) {
+                if (hashtableobj.containsKey(word)) {
+                    Bag testbag = hashtableobj.get(word);
+                    testbag.add(Integer.parseInt(tokens[0]));
+                    hashtableobj.put(word, testbag);
+                } else {
+                    hashtableobj.put(word, new Bag<Integer>());
+                    Bag testbag = hashtableobj.get(word);
+                    testbag.add(Integer.parseInt(tokens[0]));
+                    hashtableobj.put(word, testbag);
+                }
+            }
+        }
+    }
+    /**
+     * { iAmFeelinglucky function }.
+     *
+     * @param      inputword  The inputword
+     *
+     * @return     { description_of_the_return_value }
+     */
+    int iAmFeelingLucky(final String inputword) {
+        if (!hashtableobj.containsKey(inputword)) {
+            return -1;
+        }
+        Bag<Integer> testbag = hashtableobj.get(inputword);
+        Double maxpr = -1.0;
+        int maxid = -1;
+        for (Integer everyid : testbag) {
+            if (pgrankobjinclass.getPR(everyid) > maxpr) {
+                maxpr = pgrankobjinclass.getPR(everyid);
+                maxid = everyid;
+            }
+        }
+        return maxid;
+    }
+    /**
+     * { printingkeys }.
+     */
+    void printkeys() {
+        for (String eachkey : hashtableobj.keySet()) {
+            System.out.println(eachkey);
+        }
+    }
 
 }
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused.
+    }
+    /**
+     * { Main function }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        // read the first line of the input to get the number of vertices
+
+        int vertexnum = Integer.parseInt(StdIn.readLine());
+        // iterate count of vertices times
+
+        // to read the adjacency list from std input
+        // and build the graph
+        Digraph graph = new Digraph(vertexnum);
+        for (int i = 0; i < vertexnum; i++) {
+            String[] edges = StdIn.readLine().split(" ");
+            for (int k = 1; k < edges.length; k++) {
+                graph.addEdge(
+                    Integer.parseInt(
+                        edges[0]), Integer.parseInt(
+                        edges[k]));
+            }
+        }
+        System.out.println(graph);
+        // Create page rank object and pass the graph object to the constructor
+
+        PageRank pgrankobj = new PageRank(graph);
+        // print the page rank object
+        System.out.println(pgrankobj);
+        // This part is only for the final test case
+
+        // File path to the web content
+        String file = "WebContent.txt";
 
 
-public class Solution {
-	public static void main(String[] args) {
-		// read the first line of the input to get the number of vertices
+        // instantiate web search object
+        // and pass the page rank object and the file path to the constructor
 
-		// iterate count of vertices times 
-		// to read the adjacency list from std input
-		// and build the graph
-		
-		
-		// Create page rank object and pass the graph object to the constructor
-		
-		// print the page rank object
-		
-		// This part is only for the final test case
-		
-		// File path to the web content
-		String file = "WebContent.txt";
-		
-		// instantiate web search object
-		// and pass the page rank object and the file path to the constructor
-		
-		// read the search queries from std in
-		// remove the q= prefix and extract the search word
-		// pass the word to iAmFeelingLucky method of web search
-		// print the return value of iAmFeelingLucky
-		Scanner sc = new Scanner(System.in);
-		int v = Integer.parseInt(sc.nextLine());
-		Digraph graph1 = new Digraph(v);
-		Digraph graph2 = new Digraph(v);
-		for (int i = 1; i <= v; i++) {
-			String[] str = sc.nextLine().split(" ");
-			for (int j = 1; j < str.length; j++) {
-				graph1.addEdge(Integer.parseInt(str[0]), Integer.parseInt(str[j]));
-				graph2.addEdge(Integer.parseInt(str[j]), Integer.parseInt(str[0]));
-			}
-		}
+        WebSearch webobj = new WebSearch(pgrankobj, file);
 
-		Double[] pr = new Double[v];
-		Double d = (1.0 / v);
-		for (int i = 0; i < v; i++) {
-			pr[i] = d;
-		}
-		for (int i = 1; i <= 1000; i++) {
-			for (int j = 0; j < v; j++) {
-				//Iterable<Integer> invertices = graph2.adj(j);
-				pr[j] = 0.0;
-				for (int k : graph2.adj(j)) {
-					pr[j] += (pr[k] / graph1.outdegree(k));
-				}
+        while (StdIn.hasNextLine()) {
+            String[] queries = StdIn.readLine().split("=");
+            System.out.println(webobj.iAmFeelingLucky(queries[1]));
+        }
+        // read the search queries from std in
+        // remove the q= prefix and extract the search word
+        // pass the word to iAmFeelingLucky method of web search
+        // print the return value of iAmFeelingLucky
 
-			}
-		}
-		for (int i = 0; i < v; i++) {
-			System.out.print(i + ": ");
-			for (int j:graph1.adj(i)) {
-				System.out.print(j + " ");
-			}
-			System.out.println();
-		}
-		for (int i = 0; i < v; i++) {
-			System.out.print(i + ": ");
-			for (int j:graph2.adj(i)) {
-				System.out.print(j + " ");
-			}
-			System.out.println();
-		}
-		for (int i = 0; i < v; i++) {
-			System.out.println(i +" - " + pr[i]);
-		}
-
-	}
+    }
 }
